817753733ecdcc518b9eaaa1816bf2478a194d4b
MRP8490 <mona.ricks84@gmail.com>
2026-04-20 14:15:43 -0400

    feat(top-sellers): implement dynamic top sellers with loading skeleton

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSellers = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+        const data = await res.json();
+        setSellers(data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
+
+    fetchSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -13,26 +35,45 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading
+                ? new Array(12).fill(0).map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <div className="skeleton-avatar"></div>
+                      </div>
+
+                      <div className="author_list_info">
+                        <div className="skeleton-text"></div>
+                        <div className="skeleton-text small"></div>
+                      </div>
+                    </li>
+                  ))
+                : sellers.slice(0, 12).map((seller, index) => (
+                    <li key={seller.id || index}>
+                      <div className="author_list_pp">
+                       <Link to={`/author/${seller.authorId}`}>
+                          <img
+                            className="pp-author"
+                            src={seller.authorImage || AuthorImage}
+                            alt={seller.authorName}
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+
+                      <div className="author_list_info">
+                        <Link
+                          to={`/author/${seller.authorId}`}
+                        >
+                          {seller.authorName}
+                        </Link>
+                        <span>{seller.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
             </ol>
           </div>
         </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,41 +1,87 @@
 import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const Author = () => {
+  const { id } = useParams();
   const location = useLocation();
-  const authorId = location.state?.authorId;
+
+  const authorId = id || location.state?.authorId || null;
 
   const [authorData, setAuthorData] = useState(null);
   const [nfts, setNfts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchAuthor = async () => {
       try {
+        if (!authorId) {
+          setLoading(false);
+          return;
+        }
+
+        setLoading(true);
+
         const res = await fetch(
           `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
         );
         const data = await res.json();
 
-        console.log("author API:", data);
-
         setAuthorData(data);
-        setNfts(data.nftCollection);
+        setNfts(data?.nftCollection || []);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    if (authorId) {
-      fetchAuthor();
-    }
+    fetchAuthor();
   }, [authorId]);
+
+  if (loading) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <section
+            id="profile_banner"
+            className="text-light"
+            style={{ background: `url(${AuthorBanner}) top` }}
+          ></section>
+
+          <section>
+            <div className="container">
+              <h3>Loading...</h3>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+
+  if (!authorId || !authorData) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <section
+            id="profile_banner"
+            className="text-light"
+            style={{ background: `url(${AuthorBanner}) top` }}
+          ></section>
+
+          <section>
+            <div className="container">
+              <h3>Author not found</h3>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-
-        {/* Banner */}
         <section
           id="profile_banner"
           className="text-light"
@@ -45,25 +91,25 @@ const Author = () => {
         <section>
           <div className="container">
             <div className="row">
-
-              {/* Profile */}
               <div className="col-md-12">
                 <div className="d_profile de-flex">
-
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={authorData?.authorImage} alt="" />
+                      <img
+                        src={authorData.authorImage}
+                        alt={authorData.authorName}
+                      />
                       <i className="fa fa-check"></i>
 
                       <div className="profile_name">
                         <h4>
-                          {authorData?.authorName}
+                          {authorData.authorName}
                           <span className="profile_username">
-                            @{authorData?.tag}
+                            @{authorData.tag}
                           </span>
 
                           <span className="profile_wallet">
-                            {authorData?.address}
+                            {authorData.address}
                           </span>
 
                           <button>Copy</button>
@@ -75,37 +121,30 @@ const Author = () => {
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
                       <div className="profile_follower">
-                        {authorData?.followers} followers
+                        {authorData.followers} followers
                       </div>
                       <Link to="#" className="btn-main">
                         Follow
                       </Link>
                     </div>
                   </div>
-
                 </div>
               </div>
 
-              {/* NFT GRID */}
               <div className="col-md-12">
                 <div className="row">
-
                   {nfts.map((nft, index) => (
                     <div
                       className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
                       key={index}
                     >
                       <div className="nft__item">
-
                         <div className="author_list_pp">
-                          <Link
-                            to="/author"
-                            state={{ authorId: authorId }}
-                          >
+                          <Link to={`/author/${authorId}`}>
                             <img
                               className="lazy"
-                              src={authorData?.authorImage}
-                              alt=""
+                              src={authorData.authorImage}
+                              alt={authorData.authorName}
                             />
                             <i className="fa fa-check"></i>
                           </Link>
@@ -115,32 +154,26 @@ const Author = () => {
                           <img
                             src={nft.nftImage}
                             className="lazy nft__item_preview"
-                            alt=""
+                            alt={nft.title}
                           />
                         </div>
 
                         <div className="nft__item_info">
                           <h4>{nft.title}</h4>
-
                           <div className="nft__item_price">{nft.price} ETH</div>
-
                           <div className="nft__item_like">
                             <i className="fa fa-heart"></i>
                             <span>{nft.likes}</span>
                           </div>
                         </div>
-
                       </div>
                     </div>
                   ))}
-
                 </div>
               </div>
-
             </div>
           </div>
         </section>
-
       </div>
     </div>
   );
