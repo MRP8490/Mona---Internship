a28c30db8e6bfbb432558d5d5b864599dcee6d5b
MRP8490 <mona.ricks84@gmail.com>
2026-04-25 12:08:47 -0400

    fix mobile slider responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -11,33 +11,27 @@ const HotCollections = () => {
   const [loading, setLoading] = useState(true);
 
   const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-        },
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
       },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-        },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
       },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
+    },
+  ],
+};
 
   useEffect(() => {
     const fetchCollections = async () => {
